f8af23db996cf59c5ade411088df195426a746c7
Victor Dupuy <victor@skillup.co>
2022-09-14 23:47:25 +0200

    multiple mocks per function implemented

--- src/mocker.test.ts
+++ src/mocker.test.ts
@@ -9,6 +9,10 @@ class Dog implements Animal {
   makeSound() {
     return "Woof!";
   }
+
+  repeatSound(sound: string) {
+    return "Woof " + sound;
+  }
 }
 
 describe("Mockit", () => {
@@ -19,6 +23,19 @@ describe("Mockit", () => {
     const mockDog = Mockit.mock(dog);
     Mockit.when(mockDog).calls("makeSound", []).thenReturn("CROAAA!");
     expect(mockDog.makeSound()).toBe("CROAAA!");
+  });
+
+  test("it should be able to mock different calls separately", () => {
+    const dog = new Dog();
+    expect(dog.repeatSound("yo")).toBe("Woof yo");
+
+    const mockDog = Mockit.mock(dog);
+    Mockit.when(mockDog).calls("repeatSound", ["A"]).thenReturn("yo");
+    Mockit.when(mockDog).calls("repeatSound", ["B"]).thenReturn("HELLAW!");
+
+    expect(mockDog.repeatSound("A")).toBe("yo");
+    expect(mockDog.repeatSound("B")).toBe("HELLAW!");
+    expect(mockDog.repeatSound("A")).toBe("yo");
   });
 
   test("it should allow to replace a throwing method by a returning one", () => {
--- src/mocker.ts
+++ src/mocker.ts
@@ -1,16 +1,57 @@
 class Mock {
+  private calls: Record<string, any> = {};
   //   private calls: {
   //     key: string;
   //     whatToDo: (...args: any[]) => any;
   //   }[] = [];
 
+  private callsMap: Map<string, any> = new Map();
   constructor() {}
+
+  registerMock(
+    functionName: string,
+    args: any[],
+    whatToDo: (...args: any[]) => any
+  ) {
+    if (!this.calls[functionName]) {
+      this.calls[functionName] = {};
+      this.callsMap.set(functionName, new Map());
+    }
+
+    this.calls[functionName][this.parseArgs(args)] = whatToDo;
+
+    const funcMockMap = this.callsMap.get(functionName);
+    funcMockMap.set(this.parseArgs(args), whatToDo);
+
+    this.callsMap.set(functionName, funcMockMap);
+
+    this.refreshMockFunction(functionName);
+  }
+
+  private parseArgs(args: any[]) {
+    return args.map((arg) => JSON.stringify(arg)).join(",");
+  }
+
+  public refreshMockFunction(name: string) {
+    this[name] = (...args: any[]) => {
+      const key = this.parseArgs(args);
+      const mockedFunction = this.calls[name][key];
+      if (mockedFunction) {
+        return mockedFunction(...args);
+      } else {
+        throw new Error(
+          `No mocked function found for ${name} with args ${args}`
+        );
+        // should i do nothing ?
+      }
+    };
+  }
 }
 
 export class Mockit {
   constructor() {}
 
-  static mock<T extends any>(clazz: T): T {
+  static mock<T extends any>(_clazz: T): T {
     return new Mock() as T;
   }
 
@@ -19,17 +60,17 @@ export class Mockit {
       calls(func: string, withArgs: any[]) {
         return {
           thenReturn(returnValue: any) {
-            mock[func] = () => returnValue;
+            mock.registerMock(func, withArgs, () => returnValue);
           },
           thenThrow(error: any extends Error ? Error : string) {
-            mock[func] = () => {
+            mock.registerMock(func, withArgs, () => {
               throw error;
-            };
+            });
           },
           thenCall(f: (...args: any[]) => any) {
-            mock[func] = () => {
+            mock.registerMock(func, withArgs, () => {
               return f();
-            };
+            });
           },
         };
       },
