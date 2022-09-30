7ea78eccd84039387b16a1d89b4c53f196503bbe
Victor Dupuy <vdcd120491@gmail.com>
2022-09-30 19:32:55 +0200

    combining default behaviours

--- src/__tests__/mock2/changeFunctionBehaviour.test.ts
+++ src/__tests__/mock2/changeFunctionBehaviour.test.ts
@@ -9,6 +9,7 @@ describe("Change function behaviour", () => {
 
         // @ts-expect-error we are giving dumb arguments to match the mock
         expect(mock.walk(1, 2, 3)).toBe(42);
+        checkDefaultBehaviourIsConserved(mock);
     });
 
     it("should be able to make a function RESOLVE 33", () => {
@@ -18,6 +19,7 @@ describe("Change function behaviour", () => {
         mock.walk(1, 2, 3)?.then((resolvedValue) => {
             expect(resolvedValue).toBe(33);
         });
+        checkDefaultBehaviourIsConserved(mock);
     });
 
     it("should be able to make a function THROW an error", () => {
@@ -25,6 +27,7 @@ describe("Change function behaviour", () => {
 
         // @ts-expect-error we are giving dumb arguments to match the mock
         expect(() => mock.walk(1, 2, 3)).toThrow("error");
+        checkDefaultBehaviourIsConserved(mock);
     });
 
     it("should be able to make a function REJECT an error", () => {
@@ -34,6 +37,7 @@ describe("Change function behaviour", () => {
         mock.walk(1, 2, 3)?.catch((error) => {
             expect(error).toBe("REJECTED");
         });
+        checkDefaultBehaviourIsConserved(mock);
     });
 
     it("should be able to make a function CALL a CALLBACK", () => {
@@ -45,5 +49,37 @@ describe("Change function behaviour", () => {
         mock.walk(1, 2, 3);
 
         expect(callback).toBeCalled();
+        checkDefaultBehaviourIsConserved(mock);
     })
 });
+
+describe("Change function behaviour combined", () => {
+    const mock = Mockit.mock2(Person);
+    Mockit.when2(mock).calls("walk").withArgs(1, 2, 3).thenReturn(42);
+    Mockit.when2(mock).calls("walk").withArgs(4, 5, 6).thenResolve(33);
+    Mockit.when2(mock).calls("walk").withArgs(7, 8, 9).thenThrow("error");
+    Mockit.when2(mock).calls("walk").withArgs(10, 11, 12).thenReject("REJECTED");
+    Mockit.when2(mock).calls("walk").withArgs(13, 14, 15).thenCall(() => {});
+
+    it("should be able to combine different behaviours", () => {
+        // @ts-expect-error we are giving dumb arguments to match the mock
+        expect(mock.walk(1, 2, 3)).toBe(42);
+        // @ts-expect-error we are giving dumb arguments to match the mock
+        mock.walk(4, 5, 6)?.then((resolvedValue) => {
+            expect(resolvedValue).toBe(33);
+        });
+        // @ts-expect-error we are giving dumb arguments to match the mock
+        expect(() => mock.walk(7, 8, 9)).toThrow("error");
+        // @ts-expect-error we are giving dumb arguments to match the mock
+        mock.walk(10, 11, 12)?.catch((error) => {
+            expect(error).toBe("REJECTED");
+        });
+        // @ts-expect-error we are giving dumb arguments to match the mock
+        mock.walk(13, 14, 15);
+        checkDefaultBehaviourIsConserved(mock);
+    });
+})
+
+function checkDefaultBehaviourIsConserved(mock: Person) {
+    expect(mock.walk()).toBeUndefined();
+}
