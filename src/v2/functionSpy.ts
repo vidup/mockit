9fedc95d8a34ca370dc856471878ea4c271a799a
Victor Dupuy <victor@skillup.co>
2023-02-27 20:00:34 +0100

    WIP any with single arg

--- src/v2/functionSpy.ts
+++ src/v2/functionSpy.ts
@@ -69,11 +69,76 @@ export class FunctionSpy {
         return callsMap.getCalls().length === howMuch;
       },
       withArgs(...args: any[]) {
-        // const argscontainZodSchema = argsContainZodSchema(...args);
-        // const calledArgs = callsMap.getArgs();
+        const argscontainZodSchema = argsContainZodSchema(...args);
+        const calledArgsList = callsMap.getArgs();
 
         return {
           get atleastOnce() {
+            if (argscontainZodSchema) {
+              let matchesWithSchema = false;
+              for (const calledArgs of calledArgsList) {
+                if (matchesWithSchema) return true;
+
+                for (let i = 0; i < args.length; i++) {
+                  const arg = args[i];
+                  /** ZOD ARGUMENT */
+                  if (arg instanceof z.ZodType) {
+                    if (arg.safeParse(calledArgs[i]).success) {
+                      matchesWithSchema = true;
+                    }
+                    continue;
+                  }
+
+                  /** SIMPLE PRIMITIVE ARGUMENT */
+                  if (
+                    arg instanceof String ||
+                    arg instanceof Number ||
+                    arg instanceof Boolean
+                  ) {
+                    if (arg === calledArgs[i]) {
+                      matchesWithSchema = true;
+                    }
+                    continue;
+                  }
+
+                  /** OBJECT OR ARRAY ARGUMENT */
+                  if (typeof arg === "object") {
+                    if (arg instanceof Map) {
+                      throw new Error("Not implemented yet");
+                    }
+
+                    if (arg instanceof Set) {
+                      throw new Error("Not implemented yet");
+                    }
+
+                    const flattenedObjects = flattenObjectOrArrays(arg);
+                    for (const [key, value] of Object.entries(
+                      flattenedObjects
+                    )) {
+                      const value = getDeepValue(key, arg);
+                      const calledValue = getDeepValue(key, calledArgs[i]);
+                      if (value instanceof z.ZodType) {
+                        if (value.safeParse(calledValue).success) {
+                          matchesWithSchema = true;
+                          continue;
+                        }
+                      }
+
+                      if (value === calledValue) {
+                        // Theorically it should be simple primitives only here
+                        matchesWithSchema = true;
+                      }
+                    }
+                  }
+                }
+
+                if (matchesWithSchema) {
+                  return true;
+                }
+              }
+
+              return false;
+            }
             return callsMap.hasBeenCalledWith(...args);
           },
           get once() {
@@ -109,3 +174,22 @@ export function argsContainZodSchema(...args: any[]) {
 
   return level0ArgscontainZodSchema || objectsContainZod;
 }
+
+function getDeepValue(path: string, obj) {
+  // path is of form x.y.z.0.a etc...
+  const pathParts = path.split(".");
+  let current = obj;
+  console.log(path);
+  for (let i = 0; i < pathParts.length; i++) {
+    if (current[pathParts[i]] === undefined) {
+      throw new Error(`Path ${path} does not exist`);
+    }
+    current = current[pathParts[i]];
+  }
+
+  return current;
+}
+
+// Now we just need to check, arg by arg, if the value is a zod schema
+// then if it is not, we just expect the value to be equal to the one passed
+// if it is, we expect the value to be valid against the schema
