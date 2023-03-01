10f1829d6f3d9f01080ac1f6e025e0619da7f318
Victor Dupuy <victor@skillup.co>
2023-03-01 19:41:10 +0100

    made it work for nTimes & sugar syntax helpers too

--- src/v2/functionSpy.ts
+++ src/v2/functionSpy.ts
@@ -75,125 +75,176 @@ export class FunctionSpy {
         return {
           get atleastOnce() {
             if (argscontainZodSchema) {
-              let allArgsMatch = expectedArgs.map((arg) => false);
-              for (
-                let CALLED_LIST_INDEX = 0;
-                CALLED_LIST_INDEX < calledArgsList.length;
-                CALLED_LIST_INDEX++
-              ) {
-                if (allArgsMatch.every((m) => m)) {
-                  // Function exit early since it's at least once.
-                  return true;
-                } else {
-                  // Reset the array to false for the next iteration
-                  allArgsMatch = allArgsMatch.map((m) => false);
-                }
-
-                // Get the args for the current analysed call
-                const calledArgs = calledArgsList[CALLED_LIST_INDEX];
-                for (
-                  let SPY_ARG_INDEX = 0;
-                  SPY_ARG_INDEX < expectedArgs.length;
-                  SPY_ARG_INDEX++
-                ) {
-                  // Here we check for each spied argument if it matches the called arg at the same index
-                  const arg = expectedArgs[SPY_ARG_INDEX];
-                  /** ZOD ARGUMENT */
-                  if (arg instanceof z.ZodType) {
-                    if (arg.safeParse(calledArgs[SPY_ARG_INDEX]).success) {
-                      allArgsMatch[SPY_ARG_INDEX] = true;
-                    }
-                    continue;
-                  }
-
-                  /** SIMPLE PRIMITIVE ARGUMENT */
-                  if (
-                    typeof arg === "string" ||
-                    typeof arg === "number" ||
-                    typeof arg === "boolean" ||
-                    typeof arg === "bigint" ||
-                    arg == null
-                  ) {
-                    if (arg === calledArgs[SPY_ARG_INDEX]) {
-                      allArgsMatch[SPY_ARG_INDEX] = true;
-                    }
-                    continue;
-                  }
-
-                  /** OBJECT OR ARRAY ARGUMENT */
-                  if (typeof arg === "object") {
-                    if (arg instanceof Map) {
-                      throw new Error("Not implemented yet");
-                    }
-
-                    if (arg instanceof Set) {
-                      throw new Error("Not implemented yet");
-                    }
-
-                    const flattenedObject = flattenObjectOrArrays(arg);
-                    // This is a flattened version of an object or array
-                    // { x: { y: 1 } } => { "x.y": 1 };
-                    // [1, 2, 3] => { "0": 1, "1": 2, "2": 3 }
-                    // Now we can iterate over the keys and check if the calledArg matches for each key
-
-                    const entries = Object.entries(flattenedObject);
-                    const objetKeysMatch = entries.map((entry) => false);
-                    for (
-                      let ENTRY_INDEX = 0;
-                      ENTRY_INDEX < entries.length;
-                      ENTRY_INDEX++
-                    ) {
-                      const [key, value] = entries[ENTRY_INDEX];
-                      if (calledArgs[SPY_ARG_INDEX] == null) {
-                        // If the called arg is null, we can't check for keys so we consider
-                        // that it does not match, and we continue to the next entry
-                        continue;
-                      }
-                      const calledValue = getDeepValue(
-                        key,
-                        calledArgs[SPY_ARG_INDEX]
-                      );
-
-                      if (value instanceof z.ZodType) {
-                        if (value.safeParse(calledValue).success) {
-                          objetKeysMatch[ENTRY_INDEX] = true;
-                          continue;
-                        }
-                      }
-
-                      if (value === calledValue) {
-                        // Theorically it should be simple primitives only here
-                        objetKeysMatch[ENTRY_INDEX] = true;
-                      }
-                    }
-
-                    if (objetKeysMatch.every((m) => m)) {
-                      allArgsMatch[SPY_ARG_INDEX] = true;
-                    }
-                  }
-                }
-              }
-
-              return allArgsMatch.every((m) => m);
+              return (
+                checkIfMatches({
+                  STOP_ONCE_MATCHES_TIMES: 1, // hehe :D
+                  expectedArgs,
+                  calledArgsList,
+                }) > 0
+              );
             }
             return callsMap.hasBeenCalledWith(...expectedArgs);
           },
           get once() {
+            if (argscontainZodSchema) {
+              return (
+                checkIfMatches({
+                  expectedArgs,
+                  calledArgsList,
+                }) === 1
+              );
+            }
+
             return callsMap.hasBeenCalledNTimesWith(1, ...expectedArgs);
           },
           get twice() {
+            if (argscontainZodSchema) {
+              return (
+                checkIfMatches({
+                  expectedArgs,
+                  calledArgsList,
+                }) === 2
+              );
+            }
+
             return callsMap.hasBeenCalledNTimesWith(2, ...expectedArgs);
           },
           get thrice() {
+            if (argscontainZodSchema) {
+              return (
+                checkIfMatches({
+                  expectedArgs,
+                  calledArgsList,
+                }) === 3
+              );
+            }
+
             return callsMap.hasBeenCalledNTimesWith(3, ...expectedArgs);
           },
           nTimes(howMuch: number) {
+            if (argscontainZodSchema) {
+              return (
+                checkIfMatches({
+                  expectedArgs,
+                  calledArgsList,
+                }) === howMuch
+              );
+            }
+
             return callsMap.hasBeenCalledNTimesWith(howMuch, ...expectedArgs);
           },
         };
       },
     };
   }
+}
+
+function checkIfMatches({
+  STOP_ONCE_MATCHES_TIMES,
+  expectedArgs,
+  calledArgsList,
+}: {
+  expectedArgs: any[];
+  calledArgsList: any[];
+  STOP_ONCE_MATCHES_TIMES?: number;
+}) {
+  let matches = 0;
+  let allArgsMatch = expectedArgs.map(() => false);
+  for (
+    let CALLED_LIST_INDEX = 0;
+    CALLED_LIST_INDEX < calledArgsList.length;
+    CALLED_LIST_INDEX++
+  ) {
+    // Reset the array to false for the next iteration
+    allArgsMatch = allArgsMatch.map(() => false);
+
+    if (STOP_ONCE_MATCHES_TIMES && matches === STOP_ONCE_MATCHES_TIMES) {
+      return true;
+    }
+
+    // Get the args for the current analysed call
+    const calledArgs = calledArgsList[CALLED_LIST_INDEX];
+    for (
+      let SPY_ARG_INDEX = 0;
+      SPY_ARG_INDEX < expectedArgs.length;
+      SPY_ARG_INDEX++
+    ) {
+      // Here we check for each spied argument if it matches the called arg at the same index
+      const arg = expectedArgs[SPY_ARG_INDEX];
+      /** ZOD ARGUMENT */
+      if (arg instanceof z.ZodType) {
+        if (arg.safeParse(calledArgs[SPY_ARG_INDEX]).success) {
+          allArgsMatch[SPY_ARG_INDEX] = true;
+        }
+        continue;
+      }
+
+      /** SIMPLE PRIMITIVE ARGUMENT */
+      if (
+        typeof arg === "string" ||
+        typeof arg === "number" ||
+        typeof arg === "boolean" ||
+        typeof arg === "bigint" ||
+        arg == null
+      ) {
+        if (arg === calledArgs[SPY_ARG_INDEX]) {
+          allArgsMatch[SPY_ARG_INDEX] = true;
+        }
+        continue;
+      }
+
+      /** OBJECT OR ARRAY ARGUMENT */
+      if (typeof arg === "object") {
+        if (arg instanceof Map) {
+          throw new Error("Not implemented yet");
+        }
+
+        if (arg instanceof Set) {
+          throw new Error("Not implemented yet");
+        }
+
+        const flattenedObject = flattenObjectOrArrays(arg);
+        // This is a flattened version of an object or array
+        // { x: { y: 1 } } => { "x.y": 1 };
+        // [1, 2, 3] => { "0": 1, "1": 2, "2": 3 }
+        // Now we can iterate over the keys and check if the calledArg matches for each key
+
+        const entries = Object.entries(flattenedObject);
+        const objetKeysMatch = entries.map((entry) => false);
+        for (let ENTRY_INDEX = 0; ENTRY_INDEX < entries.length; ENTRY_INDEX++) {
+          const [key, value] = entries[ENTRY_INDEX];
+          if (calledArgs[SPY_ARG_INDEX] == null) {
+            // If the called arg is null, we can't check for keys so we consider
+            // that it does not match, and we continue to the next entry
+            continue;
+          }
+          const calledValue = getDeepValue(key, calledArgs[SPY_ARG_INDEX]);
+
+          if (value instanceof z.ZodType) {
+            if (value.safeParse(calledValue).success) {
+              objetKeysMatch[ENTRY_INDEX] = true;
+              continue;
+            }
+          }
+
+          if (value === calledValue) {
+            // Theorically it should be simple primitives only here
+            objetKeysMatch[ENTRY_INDEX] = true;
+          }
+        }
+
+        if (objetKeysMatch.every((m) => m)) {
+          allArgsMatch[SPY_ARG_INDEX] = true;
+        }
+      }
+    }
+
+    if (allArgsMatch.every((m) => m)) {
+      // Function exit early since it's at least once.
+      matches += 1;
+    }
+  }
+  return matches;
 }
 
 export function argsContainZodSchema(...args: any[]) {
---
+++ src/v2/spy.any.nTimes.spec.ts
@@ -0,0 +1,106 @@
+import { Mockit } from "./Mockit";
+
+export function hello(...args: any[]) {}
+
+describe("v2 spy.any.nTimes", () => {
+  it("should work for atLeastOnce", () => {
+    const mock = Mockit.mockFunction(hello);
+    const spy = Mockit.spy(mock);
+
+    expect(
+      spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).atleastOnce
+    ).toBe(false);
+    mock({ x: "hello" });
+    expect(
+      spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).atleastOnce
+    ).toBe(true);
+    mock({ x: "hello" });
+    mock({ x: "1" });
+    expect(
+      spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).atleastOnce
+    ).toBe(true);
+  });
+
+  it("should work for only once", () => {
+    const mock = Mockit.mockFunction(hello);
+    const spy = Mockit.spy(mock);
+
+    mock({ x: "hello" });
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).once).toBe(
+      true
+    );
+    mock({ x: "hello" });
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).once).toBe(
+      false
+    );
+  });
+
+  it("should work for twice", () => {
+    const mock = Mockit.mockFunction(hello);
+    const spy = Mockit.spy(mock);
+
+    mock({ x: "hello" });
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).twice).toBe(
+      false
+    );
+
+    mock({ x: "hello" });
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).twice).toBe(
+      true
+    );
+
+    mock({ x: "hello" });
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).twice).toBe(
+      false
+    );
+  });
+
+  it("should work for thrice", () => {
+    const mock = Mockit.mockFunction(hello);
+    const spy = Mockit.spy(mock);
+
+    mock({ x: "hello" });
+    mock({ x: "hello" });
+
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).thrice).toBe(
+      false
+    );
+
+    mock({ x: "hello" });
+
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).thrice).toBe(
+      true
+    );
+
+    mock({ x: "hello" });
+
+    expect(spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).thrice).toBe(
+      false
+    );
+  });
+
+  for (const n of [1, 2, 3, 4, 7, 22, 100]) {
+    it(`should work for nTimes(${n})`, () => {
+      const mock = Mockit.mockFunction(hello);
+      const spy = Mockit.spy(mock);
+
+      expect(
+        spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).nTimes(n)
+      ).toBe(false);
+
+      for (let i = 0; i < n; i++) {
+        mock({ x: "hello" });
+      }
+
+      expect(
+        spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).nTimes(n)
+      ).toBe(true);
+
+      mock({ x: "hello" });
+
+      expect(
+        spy.hasBeenCalled.withArgs({ x: Mockit.any.string }).nTimes(n)
+      ).toBe(false);
+    });
+  }
+});
